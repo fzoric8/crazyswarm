f1a36d3cfc38503765c174a5843c588f9e90d559
Filip Zoric <fzoric8@gmail.com>
2024-07-23 17:17:49 +0000

    Added mocap_helper update to enable tracking of the eliso3 and the elios3ee

--- ros_ws/src/crazyswarm/src/mocap_helper.cpp
+++ ros_ws/src/crazyswarm/src/mocap_helper.cpp
@@ -3,6 +3,8 @@
 
 #include <ros/ros.h>
 
+#include <geometry_msgs/PoseStamped.h>
+
 // Motion Capture
 #include <libmotioncapture/motioncapture.h>
 
@@ -11,6 +13,13 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "mocap_helper");
 
   ros::NodeHandle nl("~");
+
+  ros::Publisher uavPosePub = nl.advertise<geometry_msgs::PoseStamped>("opt_elios_pose", 1);
+  ros::Publisher eePosePub = nl.advertise<geometry_msgs::PoseStamped>("opt_ee_pose", 1);
+
+  // get UAV and the ee name
+  std::string uavName("elios3"); 
+  std::string eeName("elios3_ee"); 
 
   std::string motionCaptureType;
   nl.param<std::string>("motion_capture_type", motionCaptureType, "vicon");
@@ -57,6 +66,39 @@ int main(int argc, char **argv)
         std::cout << "       position: [" << position(0) << ", " << position(1) << ", " << position(2) << "]" << std::endl;
         std::cout << "       rotation: [" << rotation.w() << ", " << rotation.vec()(0) << ", "
                                           << rotation.vec()(1) << ", " << rotation.vec()(2) << "]" << std::endl;
+        
+        std::string bodyName(body.name());
+
+        if (bodyName.compare(uavName) == 0){ 
+          geometry_msgs::PoseStamped uavPoseMsg;
+          uavPoseMsg.header.stamp = ros::Time::now();
+          uavPoseMsg.header.frame_id = "world";
+          uavPoseMsg.pose.position.x = position(0);
+          uavPoseMsg.pose.position.y = position(1);
+          uavPoseMsg.pose.position.z = position(2);
+          uavPoseMsg.pose.orientation.w = rotation.w();
+          uavPoseMsg.pose.orientation.x = rotation.vec()(0);
+          uavPoseMsg.pose.orientation.y = rotation.vec()(1);
+          uavPoseMsg.pose.orientation.z = rotation.vec()(2);
+          uavPosePub.publish(uavPoseMsg);
+        }
+        else if (bodyName.compare(eeName) == 0)
+        {
+          geometry_msgs::PoseStamped eePoseMsg;
+          eePoseMsg.header.stamp = ros::Time::now();
+          eePoseMsg.header.frame_id = "world";
+          eePoseMsg.pose.position.x = position(0);
+          eePoseMsg.pose.position.y = position(1);
+          eePoseMsg.pose.position.z = position(2);
+          eePoseMsg.pose.orientation.w = rotation.w();
+          eePoseMsg.pose.orientation.x = rotation.vec()(0);
+          eePoseMsg.pose.orientation.y = rotation.vec()(1);
+          eePoseMsg.pose.orientation.z = rotation.vec()(2);
+          eePosePub.publish(eePoseMsg);
+        }
+        else{
+          std::cout << "I didn't found: " << bodyName << std::endl; 
+        }
       }
     }
 
